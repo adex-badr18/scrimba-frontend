ca96903c8a549b7962634ca2be0981653824ca5c
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-06 07:53:59 +0100

    Create JavaScript function to save & display count

--- passenger-counter-app/js/script.js
+++ passenger-counter-app/js/script.js
@@ -1,7 +1,17 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
     count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+
+    // Re-initialize count
+    count = 0;
     countEl.textContent = count;
 }
