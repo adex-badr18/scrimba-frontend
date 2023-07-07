06b601d43065babd65d5e85a91bfd592a4718606
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-07 23:30:46 +0100

    Create function that adds 2 points to home score

--- basketball-scoreboard/js/script.js
+++ basketball-scoreboard/js/script.js
@@ -7,3 +7,8 @@ function add1PointHome() {
     homeCount += 1;
     homeScore.textContent = homeCount;
 }
+
+function add2PointsHome() {
+    homeCount += 2;
+    homeScore.textContent = homeCount;
+}
