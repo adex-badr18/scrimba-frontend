b011bb1061020a818b337b87f8a736115329c1be
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-08 21:32:44 +0100

    Create function that highlights the leader

--- basketball-scoreboard/js/script.js
+++ basketball-scoreboard/js/script.js
@@ -1,39 +1,62 @@
-let homeScore = document.getElementById("home-score");
-let guestScore = document.getElementById("guest-score");
+let homeScoreEl = document.getElementById("home-score");
+let guestScoreEl = document.getElementById("guest-score");
 let homeCount = 0;
 let guestCount = 0;
 
+function markHigherScore() {
+    let homeScore = Number(homeScoreEl.textContent);
+    let guestScore = Number(guestScoreEl.textContent);
+    let home = document.getElementById('home-score-container');
+    let guest = document.getElementById('guest-score-container');
+
+    if (homeScore > guestScore) {
+        home.style.border = '3px solid #00ff00';
+        guest.style.removeProperty('border');
+    } 
+
+    if (guestScore > homeScore) {
+        guest.style.border = '3px solid #00ff00';
+        home.style.removeProperty('border');
+    }
+}
+
 function add1PointHome() {
     homeCount += 1;
-    homeScore.textContent = homeCount;
+    homeScoreEl.textContent = homeCount;
+    markHigherScore();
 }
 
 function add2PointsHome() {
     homeCount += 2;
-    homeScore.textContent = homeCount;
+    homeScoreEl.textContent = homeCount;
+    markHigherScore();
 }
 
 function add3PointsHome() {
     homeCount += 3;
-    homeScore.textContent = homeCount;
+    homeScoreEl.textContent = homeCount;
+    markHigherScore();
 }
 
 // Guest score functions
 function add1PointGuest() {
     guestCount += 1;
-    guestScore.textContent = guestCount;
+    guestScoreEl.textContent = guestCount;
+    markHigherScore();
 }
 
 function add2PointsGuest() {
     guestCount += 2;
-    guestScore.textContent = guestCount;
+    guestScoreEl.textContent = guestCount;
+    markHigherScore();
 }
 
 function add3PointsGuest() {
     guestCount += 3;
-    guestScore.textContent = guestCount;
+    guestScoreEl.textContent = guestCount;
+    markHigherScore();
 }
 
-function markHigherScore() {
-    
-}
+
+// EventListener
+document.querySelector('button').addEventListener('click', markHigherScore());
