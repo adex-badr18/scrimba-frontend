1865b6e7cbf921179c02b434ff8b5acac0158eef
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-11 14:44:34 +0100

    Use if conditionals to structure the logic of the game

--- blackjack-game/js/script.js
+++ blackjack-game/js/script.js
@@ -8,4 +8,10 @@ let secondCard = 9;
 
 let sum = firstCard + secondCard;
 
-console.log(sum);
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
