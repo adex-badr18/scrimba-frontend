f1c5fad94c539d7952dfdfcf200a8d8b2dbf6ef0
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-11 16:59:20 +0100

    Create a startGame() function that calls renderGame()

--- blackjack-game/js/script.js
+++ blackjack-game/js/script.js
@@ -16,8 +16,12 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 
-// Write the conditional according to these rules:
+// Create a new function called startGame() that calls renderGame()
+function startGame() {
+    renderGame()
+}
 
+// Write the conditional according to these rules:
 // if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
 // else -> "You're out of the game! 😭"
