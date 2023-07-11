1ec9ffb0e8a12638f5edd7abdd09c96b8a7dea24
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-11 16:52:24 +0100

    Create newCard() function that logs message to console

--- blackjack-game/js/script.js
+++ blackjack-game/js/script.js
@@ -42,3 +42,8 @@ function startGame() {
     // Display the message in the messageEl using messageEl.textContent
     messageEl.textContent = message;
 }
+
+// Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+}
