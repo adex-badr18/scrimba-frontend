c07c90c48ae8d4d68c1422eef11e04ff12150692
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-16 17:10:52 +0100

    Get the leads from localStorage

--- lead-tracker-extension/js/script.js
+++ lead-tracker-extension/js/script.js
@@ -1,7 +1,9 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el");
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
@@ -15,9 +17,6 @@ inputBtn.addEventListener("click", function () {
 
 
 function renderLeads() {
-    // myLeads = localStorage.getItem("myLeads");
-    // myLeads = JSON.parse(myLeads);
-
     let listItems = '';
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
