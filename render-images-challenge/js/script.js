71b96419ec6f5984e0060bacc33c43153345e1d3
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-17 17:10:46 +0100

    Create the render() function

--- render-images-challenge/js/script.js
+++ render-images-challenge/js/script.js
@@ -6,3 +6,14 @@ const imgs = [
 
 const containerEl = document.getElementById("container");
 
+function render(images) {
+    let output = ""
+    for (let i = 0; i < images.length; i++) {
+        output += `
+            <img class="team-img" src="${images[i]}">
+        `
+    }
+    
+    containerEl.innerHTML = output
+}
+
