192f18fa7ddf80d7f03e5f5e7645e6dfc657125a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-18 17:44:34 +0100

    Create variables that hold DOM elements

---
+++ unit-converter/js/script.js
@@ -0,0 +1,5 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const lengthOutput = document.getElementById("length-output");
+const volumeOutput = document.getElementById("volume-output");
+const kiloOutput = document.getElementById("kilo-output");
