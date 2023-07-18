7ee8bf3451659d41996addb5a55d1d08baec4b1e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-18 21:22:46 +0100

    Create function for length conversion

--- unit-converter/js/script.js
+++ unit-converter/js/script.js
@@ -1,5 +1,20 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
-const lengthOutput = document.getElementById("length-output");
-const volumeOutput = document.getElementById("volume-output");
-const kiloOutput = document.getElementById("kilo-output");
+const lengthOutputEl = document.getElementById("length-output");
+const volumeOutputEl = document.getElementById("volume-output");
+const massOutputEl = document.getElementById("kilo-output");
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+value = Number(numberInput.value);
+
+function lengthConversion(num) {
+    const meter = num * 3.281;
+    const feet = num / 3.281;
+    return [meter, feet];
+}
+
