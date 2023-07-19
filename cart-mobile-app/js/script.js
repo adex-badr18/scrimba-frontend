b6226903e2f5592d9bef5a56609e6d9d17266e68
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-19 18:56:47 +0100

    Setup & create database for the project

--- cart-mobile-app/js/script.js
+++ cart-mobile-app/js/script.js
@@ -1,9 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-dd256-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const addBtn = document.getElementById("add-button");
 const inputField = document.getElementById("input-field");
