8a2e4e3b2268f1ca1abb533f9f31e227211b5210
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-07-22 21:52:42 +0100

    Show error message if textarea is empty on button click

--- endorse-peer-app/js/script.js
+++ endorse-peer-app/js/script.js
@@ -1,1 +1,15 @@
-// javascript
+const textareaEl = document.getElementById("endorsement-text");
+const publishButton = document.getElementById("publish-btn");
+const errorMessageEl = document.getElementById("error-message-el");
+
+publishButton.addEventListener("click", () => {
+    if (textareaEl.value !== "") {
+        console.log("Not empty");
+    } else {
+        showErrorMessage();
+    }
+})
+
+function showErrorMessage() {
+    errorMessageEl.textContent = "Endorsement field should not be empty";
+}
