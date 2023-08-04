9c91d05d8f7747af4e9726241d5dde3a8808c6ad
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-04 15:25:36 +0100

    Adjust social icons sizes

--- oldagram/js/script.js
+++ oldagram/js/script.js
@@ -26,5 +26,42 @@ const posts = [
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
-]
+];
+const postContainer = document.getElementById("post-container");
+
+const postEl = document.createElement('div');
+postEl.className = "user-post-wrapper";
+
+// userInfoWrapper markup
+const userInfoWrapper = document.createElement('div');
+userInfoWrapper.className = "user-info-wrapper wrapper";
+
+const userAvatarImg = document.createElement('img');
+userAvatarImg.className = "user-avatar";
+userAvatarImg.setAttribute('src', `${posts[0].avatar}`);
+userAvatarImg.setAttribute('alt', 'User avatar in a post');
+
+const nameLocationBox = document.createElement('div');
+nameLocationBox.className = 'name-location-box';
+
+const nameEl = document.createElement('h3');
+nameEl.textContent = `${posts[0].name}`;
+
+const locationEl = document.createElement('p');
+locationEl.textContent = `${posts[0].location}`;
+
+// postImageWrapper markup
+const postImageWrapper = document.createElement('div');
+postImageWrapper.className = 'post-image-wrapper';
+
+const postImage = document.createElement('img');
+postImage.className = 'post-img';
+postImage.setAttribute('src', `${posts[0].post}`);
+postImage.setAttribute('alt', 'Posted image');
+
+
+nameLocationBox.append(nameEl, locationEl);
+userInfoWrapper.append(userAvatarImg, nameLocationBox);
+
+
 
