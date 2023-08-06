10bb7902072a2125e1eee92043fd9382c407aa82
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-06 14:08:00 +0100

    Grab form data on form submission

--- cookie-consent/js/script.js
+++ cookie-consent/js/script.js
@@ -14,6 +14,9 @@ modalCloseBtn.addEventListener('click', function () {
 consentForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
+    const consentFormData = new FormData(consentForm);
+    console.log(consentFormData);
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
