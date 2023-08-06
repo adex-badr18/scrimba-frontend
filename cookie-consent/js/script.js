c325e15894a39dc2d8fdef8c3f8db9033a0cdee1
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-06 15:21:33 +0100

    Log message when mouse hovers over decline btn

--- cookie-consent/js/script.js
+++ cookie-consent/js/script.js
@@ -2,6 +2,7 @@ const modal = document.getElementById('modal');
 const modalCloseBtn = document.getElementById('modal-close-btn');
 const consentForm = document.getElementById('consent-form');
 const modalText = document.getElementById('modal-text');
+const declineBtn = document.getElementById('decline-btn');
 
 setTimeout(function () {
     modal.style.display = 'inline';
@@ -9,6 +10,10 @@ setTimeout(function () {
 
 modalCloseBtn.addEventListener('click', function () {
     modal.style.display = 'none';
+});
+
+declineBtn.addEventListener('mouseover', () => {
+    console.log("hovered");
 });
 
 consentForm.addEventListener('submit', function (e) {
