03b2af8736179c711429c9c11edba6a2e23ec2b8
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-06 14:20:38 +0100

    Add a span tag name holder in modal message

--- cookie-consent/js/script.js
+++ cookie-consent/js/script.js
@@ -31,7 +31,7 @@ consentForm.addEventListener('submit', function (e) {
 
     setTimeout(function () {
         document.getElementById('modal-inner').innerHTML = `
-            <h2>Thanks you sucker! </h2>
+            <h2>Thanks <span class="modal-display-name">NAME</span>, you sucker! </h2>
             <p>We just sold the rights to your eternal soul.</p>
             <div class="idiot-gif">
                 <img src="./images/pirate.gif">
