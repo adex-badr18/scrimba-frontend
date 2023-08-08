9d04190c071cfff26f5d770b9e1d4b4a709bd2c8
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-08 15:45:26 +0100

    Return a random cat obj if array length is above 1

--- meme-picker/js/script.js
+++ meme-picker/js/script.js
@@ -30,7 +30,10 @@ function getSingleCatObject(){
     const catsArray = getMatchingCatsArray();
 
     if (catsArray.length === 1) {
-        console.log(catsArray[0]);
+        return catsArray[0];
+    } else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length);
+        return catsArray[randomNumber];
     }
 }
 
