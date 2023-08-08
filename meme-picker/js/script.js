7d0a8f346a27de79c864f8772fb86ad0c5f4eeb4
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-08 11:48:59 +0100

    Log value of selected radio on button click

--- meme-picker/js/script.js
+++ meme-picker/js/script.js
@@ -1,6 +1,14 @@
 import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById('get-image-btn');
+
+getImageBtn.addEventListener('click', getMatchingCatsArray);
+
+function getMatchingCatsArray() {
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value;
+    console.log(selectedEmotion);
+}
 
 emotionRadios.addEventListener('change', highlightCheckedOption);
 
