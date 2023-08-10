0862cebdebdfb73781bb2ad18e4e79d22d9c0c56
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-10 17:11:48 +0100

    Implement retweet functionality

--- twitter-clone/js/script.js
+++ twitter-clone/js/script.js
@@ -12,6 +12,8 @@ tweetBtn.addEventListener('click', function () {
 document.addEventListener('click', (e) => {
     if (e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like);
+    } else if (e.target.dataset.retweet) {
+        handleRetweetClick(e.target.dataset.retweet);
     }
 })
 
@@ -27,6 +29,22 @@ function handleLikeClick(tweetId) {
     }
 
     targetTweetObj.isLiked = !targetTweetObj.isLiked;
+
+    render();
+}
+
+function handleRetweetClick(tweetId) {
+    const targetTweetObj = tweetsData.filter(tweet => {
+        return tweet.uuid === tweetId;
+    })[0];
+
+    if (targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets--;
+    } else {
+        targetTweetObj.retweets++
+    }
+
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
 
     render();
 }
