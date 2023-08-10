1fd26208be04c329c9c641812d63e3ab25b57fa7
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-10 02:22:25 +0100

    Take control of button & textarea

--- twitter-clone/js/script.js
+++ twitter-clone/js/script.js
@@ -1,3 +1,13 @@
 import { tweetsData } from "./data.js";
 
-console.log(tweetsData);
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+/*
+Challenge
+1. Have the eventListener's function
+   log out the contents of the textarea
+   when the Tweet button is clicked.
+*/
+})
