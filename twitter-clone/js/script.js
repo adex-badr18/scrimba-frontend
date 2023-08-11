16c50a17dc1eed3582de865d431b3ee2ed25053e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-11 11:26:40 +0100

    Iterate through replies & build reply markup

--- twitter-clone/js/script.js
+++ twitter-clone/js/script.js
@@ -67,26 +67,20 @@ function getFeedHtml() {
         let repliesHtml = '';
 
         if (tweet.replies.length) {
-            console.log(tweet.uuid);
+            tweet.replies.forEach(reply => {
+                repliesHtml += `
+                    <div class="tweet-reply">
+                        <div class="tweet-inner">
+                            <img src="./images/${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            })
         }
-
-        /*
-Challenge:
-1. If a tweet has replies, iterate through the replies
-   and wrap each one in the HTML template provided below. 
-   Make sure to replace words in UPPERCASE with data from 
-   the tweet. On each iteration, add this HTML to repliesHtml.
-   
-<div class="tweet-reply">
-    <div class="tweet-inner">
-        <img src="PROFILE PIC" class="profile-pic">
-            <div>
-                <p class="handle">HANDLE</p>
-                <p class="tweet-text">TWEET TEXT</p>
-            </div>
-        </div>
-</div>
-*/
 
         feedHtml += `
             <div class="tweet">
