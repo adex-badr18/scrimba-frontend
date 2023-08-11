8cc0f81175b47a0a5433fa8fb017ee4dbd6ae1ca
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-11 12:10:34 +0100

    Hide & show replies when reply button is clicked

--- twitter-clone/js/script.js
+++ twitter-clone/js/script.js
@@ -14,8 +14,14 @@ document.addEventListener('click', (e) => {
         handleLikeClick(e.target.dataset.like);
     } else if (e.target.dataset.retweet) {
         handleRetweetClick(e.target.dataset.retweet);
+    } else if (e.target.dataset.reply) {
+        handleReplyClick(e.target.dataset.reply);
     }
-})
+});
+
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden');
+}
 
 function handleLikeClick(tweetId) {
     const targetTweetObj = tweetsData.filter((tweet) => {
@@ -106,7 +112,6 @@ function getFeedHtml() {
                     </div>            
                 </div>
                 <div class="hidden" id="replies-${tweet.uuid}">
-                    <!-- REPLIES HERE -->
                     ${repliesHtml}
                 </div> 
             </div>
