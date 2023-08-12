7dd144c4039b91dac162cbb8ca2b4df95d9012c9
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-12 15:35:12 +0100

    Store & retrieve tweetsData from localStorage

--- twitter-clone/js/script.js
+++ twitter-clone/js/script.js
@@ -1,5 +1,11 @@
-import { tweetsData } from "./data.js";
+import { tweetsData as initialTweetsData } from "./data.js";
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+let tweetsData = [];
+
+localStorage.setItem("tweetsData", JSON.stringify(initialTweetsData));
+
+tweetsData = JSON.parse(localStorage.getItem("tweetsData"));
 
 document.addEventListener('click', (e) => {
     if (e.target.dataset.like) {
