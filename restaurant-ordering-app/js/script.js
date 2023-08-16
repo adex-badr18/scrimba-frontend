a18ebc767fdfa26e4f0fcf5c8fbf5717f7e8a848
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-16 16:35:31 +0100

    Add function that builds up menu list markup

--- restaurant-ordering-app/js/script.js
+++ restaurant-ordering-app/js/script.js
@@ -2,4 +2,26 @@ import { menuArray } from "./data.js";
 
 let menuData = menuArray;
 
-console.log(menuData)
+function getMenuHtml() {
+    let menuHtml = '';
+
+    menuData.forEach(menu => {
+        const ingredients = menu.ingredients.join(', ');
+        menuHtml = `
+            <div class="menu-item">
+                <div class="menu-image">${menu.emoji}</div>
+                <div class="menu-details">
+                    <h3>${menu.name}</h3>
+                    <p>${ingredients}</p>
+                    <h4>$${menu.price}</h4>
+                </div>
+                <div class="icon-wrapper">
+                    <i class="fa-regular fa-plus add-icon" id="icon-${menu.id}" data-add="${menu.id}"></i>
+                </div>
+            </div>
+        `;
+    });
+
+    return menuHtml;
+}
+
