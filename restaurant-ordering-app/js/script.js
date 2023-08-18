e2f36ed02fb616d2466a038ca7ecfc958a67e199
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-18 12:59:32 +0100

    Fix bugs in adding & rendering order

--- restaurant-ordering-app/js/script.js
+++ restaurant-ordering-app/js/script.js
@@ -15,7 +15,8 @@ document.addEventListener('click', e => {
 });
 
 function addMenu(menuId) {
-    let orderHtml = '';
+    let orderData = '';
+    const totalRow = document.getElementById('total');
     const targetMenuObj = menuData.filter(menu => {
         return menu.id === Number(menuId);
     })[0];
@@ -23,36 +24,39 @@ function addMenu(menuId) {
     if (!orderArray.includes(targetMenuObj)) {
         targetMenuObj.qty = 1;
         orderArray.push(targetMenuObj);
-        console.log(orderArray)
+
+        orderArray.forEach(order => {
+            orderData = `
+                <tr>
+                    <td class="meal-name">
+                        <h3>${order.name}</h3>
+                    </td>
+                    <td>
+                        <h4>$${order.price}</h4>
+                    </td>
+                    <td class="qty-col">
+                        <i class="fa-solid fa-minus decrease-qty"></i>
+                        <input type="text" class="qty-input" id="qty-${order.id}" value="${order.qty}" min="1" readonly>
+                        <i class="fa-regular fa-plus increase-qty"></i>
+                    </td>
+                    <td>
+                        <h4>$${order.price * order.qty}</h4>
+                    </td>
+                    <td><i class="fa-solid fa-trash-can delete-order"></i></td>
+                </tr>
+            `;
+        });
+        totalRow.insertAdjacentHTML('beforebegin', orderData);
     } else {
         targetMenuObj.qty++;
-        console.log(targetMenuObj)
+        const quantityInputsArray = [...document.querySelectorAll('input.qty-input')];
+
+        for (const input of quantityInputsArray) {
+            if (input.id === `qty-${menuId}`) {
+                input.value = targetMenuObj.qty
+            }
+        }
     }
-
-    orderArray.forEach(order => {
-        orderHtml += `
-            <tr>
-                <td class="meal-name">
-                    <h3>${order.name}</h3>
-                </td>
-                <td>
-                    <h4>$${order.price}</h4>
-                </td>
-                <td class="qty-col">
-                    <i class="fa-solid fa-minus decrease-qty"></i>
-                    <input type="text" class="qty-input" id="qty-${order.id}" value="${order.qty}" min="1" readonly>
-                    <i class="fa-regular fa-plus increase-qty"></i>
-                </td>
-                <td>
-                    <h4>$${order.price * order.qty}</h4>
-                </td>
-                <td><i class="fa-solid fa-trash-can delete-order"></i></td>
-            </tr>
-        `;
-
-    })
-
-    document.getElementById('order-list').innerHTML = orderHtml;
 }
 
 function getOrderHtml(menuObj) {
