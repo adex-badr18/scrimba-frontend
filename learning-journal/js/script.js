59ceb09f1a0a3013861af36df29399fa1b32b803
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-24 18:35:26 +0100

    Remove script that adds mobile menu dynamically

--- learning-journal/js/script.js
+++ learning-journal/js/script.js
@@ -1,5 +1,10 @@
+// const navMenu = document.getElementById('nav-menu');
+const nav = document.getElementById('nav');
+
 document.addEventListener('click', (e) => {
     if (e.target.classList.contains('post')) {
         location.href = './post-view.html'
+    } else if (e.target.classList.contains('fa-bars')) {
+        
     }
 })
