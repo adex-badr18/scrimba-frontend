b4fe34b9ba92d1303667c59ed8c78332feb83d6a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-25 03:49:37 +0100

    Toggle mobile-menu/close-menu icons display on click

--- learning-journal/js/script.js
+++ learning-journal/js/script.js
@@ -1,10 +1,20 @@
-// const navMenu = document.getElementById('nav-menu');
 const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const closeMenuBtn = document.getElementById('close-menu-btn');
 
 document.addEventListener('click', (e) => {
     if (e.target.classList.contains('post')) {
         location.href = './post-view.html'
     } else if (e.target.classList.contains('fa-bars')) {
         mobileMenu.classList.toggle('hidden');
+
+        mobileMenuBtn.style.display = 'none';
+        closeMenuBtn.style.display = 'block';
+        
+    } else if (e.target.classList.contains('fa-xmark')) {
+        mobileMenu.classList.toggle('hidden');
+
+        mobileMenuBtn.style.display = 'block';
+        closeMenuBtn.style.display = 'none';
     }
 })
