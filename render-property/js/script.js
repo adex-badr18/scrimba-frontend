61c8af0b923bdcc6de9a1dcdf50a171382be3656
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-27 22:24:46 +0100

    Return a string of card markup

--- render-property/js/script.js
+++ render-property/js/script.js
@@ -12,5 +12,17 @@ function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
          } = property;
 
          const totalArea = roomsM2.reduce((total, roomSize) => total + roomSize);
-    })
+
+         return `
+            <section class="card">
+                <img src="/images/${image}">
+                <div class="card-right">
+                    <h2>${propertyLocation}</h2>
+                    <h3>${priceGBP}</h3>
+                    <p>${comment}</p>
+                    <h3>${totalRoomSizeM2} m&sup2;</h3>
+                </div>
+            </section>
+         `;
+    }).join(' ');
 }
