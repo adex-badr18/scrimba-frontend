d806d23cc6aeff1e856462f312f235f16daead8d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-27 22:50:47 +0100

    Fix image broken link

--- render-property/js/script.js
+++ render-property/js/script.js
@@ -15,7 +15,7 @@ function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
 
          return `
             <section class="card">
-                <img src="../images/${image}">
+                <img src="./images/${image}">
                 <div class="card-right">
                     <h2>${propertyLocation}</h2>
                     <h3>${priceGBP}</h3>
