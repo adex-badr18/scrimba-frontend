d18745009d6b9c2a3b36d50dcf1d57e488b1d50a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-27 22:19:53 +0100

    Define getPropertyHtml function & destructure object

--- render-property/js/script.js
+++ render-property/js/script.js
@@ -1,3 +1,14 @@
 import placeholderPropertyObj from "../properties/placeholderPropertyObj";
 import propertyForSaleArr from "../properties/propertyForSaleArr";
 
+function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
+    return propertyArr.map(property => {
+        const { 
+            propertyLocation,
+            priceGBP,
+            roomsM2,
+            comment,
+            image
+         } = property;
+    })
+}
