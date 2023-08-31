7f7ab5ab89aa205fb904367cadd71700f30654da
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-31 14:57:12 +0100

    Show title & body of retrieved posts on page

--- blog-space/js/script.js
+++ blog-space/js/script.js
@@ -1,6 +1,20 @@
+const postsContainer = document.getElementById('posts-container');
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts', {method: 'GET'})
     .then(res => res.json())
     .then(data => {
         const postsArray = data.slice(0, 5);
-        console.log(postsArray);
+        
+        const postsHtml = postsArray.map((post, index) => {
+            return `
+                <div class="post">
+                    <h2 class="post-title">${post.title}</h2>
+                    <p class="post-body">${post.body}</p>
+                </div>
+
+                ${index < postsArray.length - 1 ? '<div class="divider"></div>' : ''}
+            `
+        }).join(' ');
+
+        postsContainer.innerHTML = postsHtml;
     });
