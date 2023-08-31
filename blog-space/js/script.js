e019e1199c5f22599550f1c478cd902004325f07
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-08-31 12:58:23 +0100

    Store the 5 posts in a variable

--- blog-space/js/script.js
+++ blog-space/js/script.js
@@ -1,3 +1,6 @@
 fetch('https://apis.scrimba.com/jsonplaceholder/posts', {method: 'GET'})
     .then(res => res.json())
-    .then(data => console.log(data.slice(0, 5)));
+    .then(data => {
+        const postsArray = data.slice(0, 5);
+        console.log(postsArray);
+    });
