2114403ae3ad9879591911a1855db77ec2666831
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-02 17:52:43 +0100

    Set border color of container to copied hex value

--- color-scheme-generator/js/script.js
+++ color-scheme-generator/js/script.js
@@ -52,6 +52,7 @@ document.addEventListener('click', (e) => {
             const copiedEl = document.createElement('span');
             copiedEl.textContent = `Color (${e.target.dataset.hex}) Copied`;
             colorCanvas.prepend(copiedEl);
+            colorCanvas.style.border = `5px solid ${e.target.dataset.hex}`;
 
             setTimeout(() => {
                 copiedEl.remove();
