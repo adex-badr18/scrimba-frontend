dd222ec6aaf2fdceb3c22e76d801b07356584e15
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-07 08:51:52 +0100

    Draw 2 cards from the deck

--- war-game/js/script.js
+++ war-game/js/script.js
@@ -9,4 +9,13 @@ function getDeck() {
         });
 }
 
+function drawCards() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        });
+}
+
 document.getElementById('get-deck').addEventListener('click', getDeck);
+document.getElementById('draw-cards').addEventListener('click', drawCards);
