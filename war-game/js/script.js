aa48300f29fa53ccba7c960f5e9d2926f77e7542
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-08 12:19:37 +0100

    Create function that determines winning card

--- war-game/js/script.js
+++ war-game/js/script.js
@@ -5,7 +5,7 @@ function getDeck() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
             deckId = data.deck_id;
         });
 }
@@ -14,8 +14,14 @@ function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            const [card1, card2] = data.cards;
+            // console.log(card1);
+            // console.log(card2);
+            // console.log(data);
             renderCards(data);
+
+            // setTimeout(showResultModal(card1, card2), 3000);
+            IdentifyWinningCard(card1, card2);
         });
 }
 
@@ -36,6 +42,22 @@ function renderCards(cardsObj) {
         cardsContainer.append(cardContainer);
     })
 }
+
+function IdentifyWinningCard(card1, card2) {
+    const cardValuesArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"];
+    const card1ValueIndex = cardValuesArray.indexOf(card1.value);
+    const card2ValueIndex = cardValuesArray.indexOf(card2.value);
+
+    if (card1ValueIndex > card2ValueIndex) {
+        return `Card1(${card1.value}) is greater than Card2(${card2.value}), Computer wins!`;
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return `Card2(${card2.value}) is greater than Card1(${card1.value}), You win!`;
+    } else {
+        return `Card1(${card1.value}) is equal to Card2(${card2.value}), War!`;
+    }
+}
+
+
 
 document.getElementById('get-deck-btn').addEventListener('click', getDeck);
 document.getElementById('draw-cards-btn').addEventListener('click', drawCards);
