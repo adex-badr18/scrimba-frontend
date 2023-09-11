046af28ccfc8d9ff9772510f091536633ad39d2a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-11 12:53:34 +0100

    Get form input & button into a variable

--- movie-watchlist/js/script.js
+++ movie-watchlist/js/script.js
@@ -1,3 +1,6 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-button');
+
 document.addEventListener('click', (e) => {
     console.log(e.target);
 })
