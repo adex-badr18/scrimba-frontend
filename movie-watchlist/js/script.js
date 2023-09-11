19c138e4fd42958e1d17b845b16a73c592a67a58
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-11 17:58:26 +0100

    Validate user search input

--- movie-watchlist/js/script.js
+++ movie-watchlist/js/script.js
@@ -1,10 +1,15 @@
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-button');
-
+const searchForm = document.getElementById('search-form');
 
 document.addEventListener('click', (e) => {
     if (e.target === searchBtn) {
         e.preventDefault();
-        console.log('Search button clicked');
+        if (searchInput.value) {
+            console.log(searchInput.value);
+        } else {
+            searchForm.classList.add('required');
+            searchInput.setAttribute('placeholder', 'Search for a movie - Field required');
+        }
     }
 })
