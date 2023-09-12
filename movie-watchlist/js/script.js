711e9261801daa8a8aef218e7f4dacdf09d5a66e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-12 16:02:39 +0100

    Fix movies array generation bug

--- movie-watchlist/js/script.js
+++ movie-watchlist/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener('click', (e) => {
     if (e.target === searchBtn) {
         e.preventDefault();
         if (searchInput.value) {
-            console.log(searchInput.value);
+            searchForm.classList.remove('required');
             searchMovies();
         } else {
             searchForm.classList.add('required');
@@ -23,21 +23,46 @@ async function searchMovies() {
 
     // console.log(searchResult.Search);
 
-    const movies = searchResult.Search.map((movie) => {
-        // console.log(movie.imdbID);
-        fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                // return {
-                //     title: data.Title,
-                //     runtime: data.Runtime,
-                //     genre: data.Genre,
-                //     plot: data.Plot,
-                //     image: data.Poster
-                // };
-            });
-    });
-    
-    // console.log(movies);
+    let movies = await Promise.all(searchResult.Search.map(async (movie) => {
+        // return await (await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`)).json();
+        const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`);
+        const movieObj = await movieResponse.json();
+
+        // return movieObj;
+        return {
+            title: movieObj.Title,
+            runtime: movieObj.Runtime,
+            genre: movieObj.Genre,
+            plot: movieObj.Plot,
+            image: movieObj.Poster,
+            rating: movieObj.imdbRating
+        };
+    }))
+
+    // searchResult.Search.forEach(async (movie) => {
+    //     // console.log(movie.imdbID);
+    //     const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`);
+    //     const movieObj = await movieResponse.json();
+    //     movies.push(movieObj);
+
+    // });
+
+    // movies.forEach(movie => console.log(movie))
+
+    console.log(movies);
+    // const moviesArr = movies.map(movie => {
+    //     return movie.title;
+    // return {
+    //     title: movie.Title,
+    //     runtime: movie.Runtime,
+    //     genre: movie.Genre,
+    //     plot: movie.Plot,
+    //     image: movie.Poster,
+    //     rating: movie.imdbRating
+    // };
+    //     });
+
+    // console.log(moviesArr);
 }
+
+function renderMovie(movie) { }
