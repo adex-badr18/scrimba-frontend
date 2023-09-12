ff8473e0810e0188392a90edd5add79fbafb34c7
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-12 16:38:08 +0100

    Render fetched movies on page

--- movie-watchlist/js/script.js
+++ movie-watchlist/js/script.js
@@ -1,6 +1,7 @@
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-button');
 const searchForm = document.getElementById('search-form');
+const movieList = document.getElementById('movie-list');
 
 document.addEventListener('click', (e) => {
     if (e.target === searchBtn) {
@@ -21,48 +22,53 @@ async function searchMovies() {
     const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`);
     const searchResult = await res.json();
 
-    // console.log(searchResult.Search);
-
     let movies = await Promise.all(searchResult.Search.map(async (movie) => {
-        // return await (await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`)).json();
         const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`);
         const movieObj = await movieResponse.json();
 
-        // return movieObj;
         return {
             title: movieObj.Title,
             runtime: movieObj.Runtime,
             genre: movieObj.Genre,
             plot: movieObj.Plot,
-            image: movieObj.Poster,
+            poster: movieObj.Poster,
             rating: movieObj.imdbRating
         };
-    }))
+    }));
 
-    // searchResult.Search.forEach(async (movie) => {
-    //     // console.log(movie.imdbID);
-    //     const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&plot=full`);
-    //     const movieObj = await movieResponse.json();
-    //     movies.push(movieObj);
-
-    // });
-
-    // movies.forEach(movie => console.log(movie))
-
-    console.log(movies);
-    // const moviesArr = movies.map(movie => {
-    //     return movie.title;
-    // return {
-    //     title: movie.Title,
-    //     runtime: movie.Runtime,
-    //     genre: movie.Genre,
-    //     plot: movie.Plot,
-    //     image: movie.Poster,
-    //     rating: movie.imdbRating
-    // };
-    //     });
-
-    // console.log(moviesArr);
+    renderMovies(movies);
 }
 
-function renderMovie(movie) { }
+function renderMovies(movies) {
+    const moviesHtml = movies.map((movie, index) => {
+        return `
+            <div class="movie-container">
+                <img src="${movie.poster}" class="movie-img" alt="${movie.title} movie poster">
+                <div class="movie-info">
+                    <div class="movie-header">
+                        <h2 class="movie-title">${movie.title}</h2>
+                        <span class="rating">
+                            <i class="fa-solid fa-star"></i> ${movie.rating}
+                        </span>
+                    </div>
+
+                    <div class="movie-sub-header">
+                        <span class="movie-runtime">${movie.runtime}</span>
+                        <span class="genre">${movie.genre}</span>
+                        <span class="add">
+                            <i class="fa-solid fa-circle-plus"></i> Watchlist
+                        </span>
+                    </div>
+
+                    <p class="movie-description">
+                    ${movie.plot}
+                    </p>
+                </div>
+            </div>
+
+            ${ index !== movies.length - 1 ? '<hr>' : ''}
+        `
+    }).join(' ');
+
+    movieList.innerHTML = moviesHtml;
+}
