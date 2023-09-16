2485a4f7ac721c2d5041ab7470515b571bfea21f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-16 11:32:37 +0100

    Display image author name on page

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -1,10 +1,13 @@
 const bodyEl = document.querySelector('body');
 
 async function setRandomBackgroundImage() {
-    const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=food');
+    const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature');
     const data = await res.json();
 
-    bodyEl.style.backgroundImage = `url(${data.urls.full})`;
+    bodyEl.style.backgroundImage = `url(${data.urls.regular})`;
+    bodyEl.innerHTML = `
+        <h3>${data.user.name}</h3>
+    `;
 }
 
 setRandomBackgroundImage();
