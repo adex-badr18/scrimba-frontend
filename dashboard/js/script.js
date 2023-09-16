d68bfb8dbe4df92b9974234859a199b73f080dad
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-16 13:34:52 +0100

    Handle promise rejection with try-catch

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -1,11 +1,15 @@
 const authorEl = document.getElementById('author');
 
 async function setRandomBackgroundImage() {
-    const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature');
-    const data = await res.json();
+    try {
+        const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=architecture');
+        const data = await res.json();
 
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    authorEl.textContent = `By: ${data.user.name}`;
+        document.body.style.backgroundImage = `url(${data.urls.regular})`;
+        authorEl.textContent = `By: ${data.user.name}`;
+    } catch (error) {
+        document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1497366754035-f200968a6e72?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTQ4NjcxNzR8&ixlib=rb-4.0.3&q=85')`
+    }
 }
 
 setRandomBackgroundImage();
