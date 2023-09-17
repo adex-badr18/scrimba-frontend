1ef943f3dab6b0f78d78cf0dc7cc30370195bb62
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-17 10:46:39 +0100

    Change bg-image types to nature

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -9,7 +9,7 @@ const city = document.getElementById('city');
 
 async function setRandomBackgroundImage() {
     try {
-        const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=architecture');
+        const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature');
         const data = await res.json();
 
         document.body.style.backgroundImage = `url(${data.urls.regular})`;
