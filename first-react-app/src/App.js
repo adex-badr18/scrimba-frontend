c0b076a808bf223e83ab595174edeb8b47164f8b
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-23 13:05:03 +0100

    Create MainContent component & import into App component

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import MainContent from './components/MainContent/MainComponent';
 
 function App() {
   return (
@@ -10,22 +11,6 @@ function App() {
       <Footer />
     </div>
   );
-}
-
-function MainContent() {
-  return (
-    <main>
-      <h1>Fun facts about React</h1>
-
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
-  )
 }
 
 export default App;
