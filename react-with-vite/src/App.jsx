7e0be026c619665c2091c4e6fe1f1578636e502e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-24 11:05:55 +0100

    Render Navbar & Main in App component

--- react-with-vite/src/App.jsx
+++ react-with-vite/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
   return (
-    <div>
-      <h3>Main goes here...</h3>
+    <div className='container'>
+      <Navbar />
+      <Main />
     </div>
   )
 }
