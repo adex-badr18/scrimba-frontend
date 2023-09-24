1b4cafcf4aa78713726610cadf0a49f6c8762d48
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-24 15:12:04 +0100

    Style the navbar

--- react-with-vite/src/components/Navbar.jsx
+++ react-with-vite/src/components/Navbar.jsx
@@ -1,5 +1,13 @@
+import reactLogo from '../assets/react.svg'
+
 export default function Navbar() {
     return (
-        <h3>Navbar goes here...</h3>
+        <header>
+            <nav>
+                <img src={reactLogo} className='logo-img' alt="Logo" />
+                <h3 className='logo-text'>ReactFacts</h3>
+                <h4 className='course-title'>React Course - Project 1</h4>
+            </nav>
+        </header>
     )
 }
