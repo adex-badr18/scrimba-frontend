3d88b106c4f568f915897f3d5f7aff83b964f759
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-24 15:42:13 +0100

    Add color & padding to main

--- react-with-vite/src/components/Main.jsx
+++ react-with-vite/src/components/Main.jsx
@@ -1,9 +1,9 @@
 export default function Main() {
     return (
         <main>
-            <h1>Fun facts about React</h1>
+            <h1 className="main-header">Fun facts about React</h1>
 
-            <ul>
+            <ul className="main-list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
